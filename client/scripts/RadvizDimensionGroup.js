b46dd823167091b28da3cf3032d3c7d1243c98cc
Yamuna Krishnamurthy <yamuna@nyu.edu>
2017-01-28 01:28:28 -0500

    Moved mdproj_react to client

--- client/scripts/RadvizDimensionGroup.js
+++
@@ -1,7 +0,0 @@
-function RadvizDimensionGroup(id, name, color, dimensions) {
-    this.id = id;
-    this.name = name;
-    this.color = color;
-    this.dimensions = dimensions;
-    this.dimensionsObjects = [];
-}
